289addbcc339cb8677146fce2b56530ba4a7e3ab
LouisProuveurUliege <you@example.com>
2024-09-06 17:28:52 +0200

    created def file for car. Still have to find a way to make it work

--- Cars/Class1.cs
+++ Cars/Class1.cs
@@ -1,6 +1,31 @@
-﻿namespace Cars
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+
+namespace LP2_Cars
 {
-    public class Class1
+    public class CompProperties_Cars : CompProperties
     {
+        public float massCapacity;
+
+        public float restEffectiveness;
+
+        public bool max1PerGroup;
+
+        public bool canChangeAssignedThingsAfterStarting;
+
+        public bool showOverallStats = true;
+
+        public SoundDef pawnLoadedSound;
+
+        public SoundDef pawnExitSound;
+
+        public CompProperties_Cars()
+        {
+            compClass = typeof(CompTransporter);
+        }
+
     }
 }
